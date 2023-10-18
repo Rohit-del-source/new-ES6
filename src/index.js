29082ede2ec7b354ad2296d8f53fd9834e43935a
Rohit-del-source <rohitverma18158920@gmail.com>
2023-10-18 16:11:40 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+//Write your code here
+class API {
+    #secure;
+
+    constructor(url,method = 'GET'){
+        this.url = url;
+        this.method = method;
+        this.#secure = url.startsWith('https');
+    }
+
+    isSecure() {
+        return this.#secure;
+    }
+
+    updateUrl(newUrl){
+        this.url = newUrl;
+        this.#secure = newUrl.startsWith('https');
+    }
+}
+
+
+
+
+//Do not remove this code
+module.exports = { API }
+
+
+const s = new API('http://api.com/api/hello')
+console. log(s.isSecure()) // false
+
+s.updateUrl('https://api. com/api/hello')
+console. log(s.isSecure()) // true
+
+console.log(s.url) // https://api. com/api/hello
+
+console.log(s.method) // GET
